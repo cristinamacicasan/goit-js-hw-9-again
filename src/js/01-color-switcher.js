7e9b0315b927c5d52ee60fce88ffaeb9bebe4598
cristinamacicasan <cmacicasan@gmail.com>
2024-02-08 14:10:48 +0200

    add code in 01-js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
 
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+let intervalId;
+
+function startColorChange() {
+  const startButton = document.querySelector('[data-start]');
+  const body = document.body;
+
+  startButton.disabled = true;
+
+  
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+
+function stopColorChange() {
+  const startButton = document.querySelector('[data-start]');
+  const body = document.body;
+
+  startButton.disabled = false;
+  clearInterval(intervalId);
+  body.style.backgroundColor = '';
+}
+
+
+
+document.querySelector('[data-start]').addEventListener('click', startColorChange);
+document.querySelector('[data-stop]').addEventListener('click', stopColorChange);
+
